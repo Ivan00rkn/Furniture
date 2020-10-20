94bf3f478ac9e0b0f3fc2511a509a7acab4461c0
borisov <hoppano@mail.ru>
2020-10-20 04:26:37 +0300

    Finish

---
+++ FurnitureOrder/Pages/Login.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FurnitureOrder.dataBase;
+
+namespace FurnitureOrder.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        MainWindow main;
+        public Login(MainWindow main)
+        {
+            this.main = main;
+            InitializeComponent();
+            generateCapture();
+        }
+
+        private void EnterClick(object sender, RoutedEventArgs e)
+        {
+            if (capchaText.Text == capcha.Text)
+            {
+                foreach (User user in main.bd.User)
+                {
+                    if (user.login == login.Text && user.password == password.Password)
+                    {
+                        try
+                        {
+                            main.hasEntered = true;
+                            main.user = user;
+                            selectRole(user);
+                            main.logOut.Visibility = Visibility.Visible;
+                        }
+                        catch (Exception error)
+                        {
+                            MessageBox.Show(error.ToString());
+                        }
+
+
+                    }
+                }
+                if (!main.hasEntered)
+                {
+                    MessageBox.Show("bad data, try again latter");
+                }
+            }
+            else
+            {
+                generateCapture();
+                MessageBox.Show("bad capture");
+            }
+
+        }
+
+        private void selectRole(User user)
+        {
+            main.hasEntered = true;
+            main.user = user;
+            if (user.role.ToLower() == "Заместитель директора".ToLower())
+            {
+                main.MainFrame.Navigate(new DeputyDirector(main));
+            }
+            else if (user.role.ToLower() == "директор".ToLower())
+            {
+                main.MainFrame.Navigate(new Director(main));
+            }
+
+            else if (user.role.ToLower() == "менеджер".ToLower())
+            {
+                main.MainFrame.Navigate(new Meneger(main));
+            }
+
+            else if (user.role.ToLower() == "мастер".ToLower())
+            {
+                main.MainFrame.Navigate(new Master(main));
+            }
+
+            else if (user.role.ToLower() == "заказчик".ToLower())
+            {
+                main.MainFrame.Navigate(new Customer(main));
+            }
+
+        }
+
+        private void generateCapture()
+        {
+            capcha.Text = "";
+            string simbols = "qwertyuiopasdfghjklzxcvbnm1234567890";
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                capcha.Text += simbols[random.Next(0, simbols.Length - 1)];
+            }
+        }
+    }
+}
---
+++ FurnitureOrder/Pages/Registration.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FurnitureOrder.dataBase;
+using System.Text.RegularExpressions;
+
+namespace FurnitureOrder.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        MainWindow main;
+        public Registration(MainWindow main)
+        {
+            this.main = main;
+            InitializeComponent();
+        }
+        private void Registration_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckPassword())
+            {
+                User user = new User();
+                user.Имя = name.Text;
+                user.Фамилия = surname.Text;
+                user.Отчество = patronymic.Text;
+                user.login = login.Text;
+                user.password = password.Password;
+                user.role = "заказчик";
+                try
+                {
+                    main.bd.User.Add(user);
+                    main.bd.SaveChanges();
+                    RegistrationFrame.Navigate(new Login(main));
+                    MessageBox.Show("you are registered");
+                }
+                catch
+                {
+                    main.bd.User.Remove(user);
+                    MessageBox.Show("Error! user already exists");
+                }
+            }
+        }
+        private bool CheckPassword()
+        {
+            if (password.Password.Length >= 6 && password.Password.Length <= 18 &&
+                !Regex.IsMatch(password.Password, @"(.)\1{2}") &&
+                Regex.IsMatch(password.Password, @"[1-9]{1}") &&
+                Regex.IsMatch(password.Password, @"[*&{}|+.]{1}"))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Password rulesl" + "\n" +
+                    "1) password should include symbol: *&{}|+." + "\n" +
+                    "2) password should include numberl" + "\n" +
+                    "3) Three identical symbols in a row cannot be repeated" + "\n" +
+                    "4) Password must be between 6 and 18 characters");
+                return false;
+            }
+        }
+    }
+}
